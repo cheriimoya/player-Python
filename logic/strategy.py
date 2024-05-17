95cca984f9103064be7f0259889add3f19cd8723
Christoph Walcher <christoph-wa@gmx.de>
2024-05-18 00:05:30 +0200

    Nr. 1

--- logic/strategy.py
+++ logic/strategy.py
@@ -1,12 +1,72 @@
-from typing import List
+from typing import List, Tuple
+
+from logic.kd_tree import KDTree
+from models.base import Base
 from models.game_state import GameState
 from models.player_action import PlayerAction
 
 
 def getdistance(x1: int, y1: int, z1: int, x2: int, y2: int, z2: int) -> int:
-    return int(((x1 - x2) ** 2 + (y1 - y1) ** 2 + (z1 - z2) ** 2) ** 0.5)
+    return int(((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2) ** 0.5)
+
+
+def get_base_distance(base1: Base, base2: Base) -> int:
+    return getdistance(base1.position.x, base1.position.y, base1.position.z, base2.position.x, base2.position.y,
+                       base2.position.z)
+
+
+class OwnState:
+
+    def __init__(self, gamestate: GameState):
+        self.unoccupied_bases = [base for base in gamestate.bases if not base.player]
+        self.unoccupied_bases_kd_tree = KDTree([[base.position.x, base.position.y, base.position.z, base] for base in self.unoccupied_bases], 3)
+        self.occupied_bases = [base for base in gamestate.bases if base.player and base.player != gamestate.game.player]
+        self.occupied_kd_tree = KDTree([[base.position.x, base.position.y, base.position.z, base] for base in self.occupied_bases], 3)
+        self.own_bases = [base for base in gamestate.bases if base.player == gamestate.game.player]
+        self.own_kd_tree = KDTree([[base.position.x, base.position.y, base.position.z, base] for base in self.own_bases], 3)
+
+
+    def get_no_brainers(self) -> List[Tuple[Base, Base]]:
+        no_brainers = []
+
+        for unoccupied_base in self.unoccupied_bases:
+            nearest_own = self.own_kd_tree.get_nearest([unoccupied_base.position.x, unoccupied_base.position.y, unoccupied_base.position.z])
+            nearest_occupied = self.occupied_kd_tree.get_nearest([unoccupied_base.position.x, unoccupied_base.position.y, unoccupied_base.position.z])
+
+
+            if getdistance(nearest_own[0], nearest_own[1], nearest_own[2], unoccupied_base.position.x, unoccupied_base.position.y, unoccupied_base.position.z) < getdistance(nearest_occupied[0], nearest_occupied[1], nearest_occupied[2], unoccupied_base.position.x, unoccupied_base.position.y, unoccupied_base.position.z):
+                no_brainers.append((unoccupied_base, nearest_own[3]))
+        return no_brainers
+
+
+    def get_nearest_bases(self) -> list[tuple[int, Base, Base]]:
+        distances_between_bases = [(get_base_distance(o, u), o, u) for u in self.unoccupied_bases for o in self.own_bases]
+        return sorted(distances_between_bases, key=lambda x: x[0])
+
 
 
 def decide(gamestate: GameState) -> List[PlayerAction]:
-    # TODO: place your logic here
-    return [PlayerAction(0, 0, 0)]
+
+    own_state = OwnState(gamestate)
+
+    actions = []
+
+
+    no_brainers = own_state.get_no_brainers()
+
+    for u, o in no_brainers:
+        if o.population == 0:
+            continue
+        actions.append(PlayerAction(o.uid, u.uid, 1))
+        o.population -= 1
+
+
+    nearest_bases = own_state.get_nearest_bases()
+
+    for n in nearest_bases:
+        if n[1].population == 0:
+            continue
+        actions.append(PlayerAction(n[1].uid, n[2].uid, 1))
+        n[1].population -= 1
+
+    return actions
